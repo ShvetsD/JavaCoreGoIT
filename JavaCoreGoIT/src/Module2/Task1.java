db14d4295e2bdd6a0f410b44de148ef211c6ab5a
AOC <ShvecD@i.ua>
2016-08-25 20:36:29 +0300

    Module2 task1.1

--- JavaCoreGoIT/src/Module2/Task1.java
+++ JavaCoreGoIT/src/Module2/Task1.java
@@ -6,8 +6,9 @@ public class Task1 {
     public static void main(String[] args) {
         int[] array = {156, 486, 4678, 546, 646, 188, 674, 133, 987, 434};
         int sum = 0;
-        for (int index : array)
+        for (int index : array) {
             sum += index;
+        }
         System.out.println(sum);
     }
 
