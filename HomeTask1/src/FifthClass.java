c7d28c577b3a8b552ea446c838cccab2e2f0e35c
AOC <ShvecD@i.ua>
2016-08-25 19:02:25 +0300

    Module2 task1.1

--- HomeTask1/src/FifthClass.java
+++
@@ -1,7 +0,0 @@
-
-public class FifthClass {
-
-    public static void main (String[] args){
-        System.out.println("My first programs on Java...");
-    }
-}
--- HomeTask1/src/SecondClass.java
+++
@@ -1,7 +0,0 @@
-
-public class SecondClass {
-
-    public static void main (String[] args){
-        System.out.println("My first programs on Java...");
-    }
-}
