5b54c7dd18a42d7e3331a95ac1967aec1282c6c5
AOC <ShvecD@i.ua>
2016-08-26 17:13:34 +0300

    Module2 task1.1 with method

--- JavaCoreGoIT/src/Module2/Task1.java
+++ JavaCoreGoIT/src/Module2/Task1.java
@@ -15,15 +15,27 @@ public class Task1 {
 
     public static int Modulus(int[] array) {
         int i = array[0];
-        int b = array[array.length-1];
-        int mod =  b%i;
-                return mod;
+        int b = array[array.length - 1];
+        int mod = b % i;
+        return mod;
 
+    }
+
+    public static int maxElement(int[] array) {
+                   int max= array[0];
+        for(int maximum : array){
+            if(max < maximum){
+                max = maximum;
+            }
+        }
+
+        return max;
     }
 
     public static void main(String[] args) {
         int[] array = {156, 486, 4678, 546, 646, 188, 674, 133, 987, 434};
         System.out.println(Sum(array));
         System.out.println(Modulus(array));
+        System.out.println(maxElement(array));
     }
 }
