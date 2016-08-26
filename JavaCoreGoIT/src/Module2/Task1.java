29b35a9c9c8b1920f33665bc9d0d5f00776adec9
AOC <ShvecD@i.ua>
2016-08-26 17:03:14 +0300

    Module2 task1.1 with method

--- JavaCoreGoIT/src/Module2/Task1.java
+++ JavaCoreGoIT/src/Module2/Task1.java
@@ -4,7 +4,7 @@ package Module2;
 import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Task1 {
-   static int Sum(int[] array) {
+    public static int Sum(int[] array) {
         int sum = 0;
         for (int index : array) {
             sum += index;
@@ -13,8 +13,17 @@ public class Task1 {
 
     }
 
+    public static int Modulus(int[] array) {
+        int i = array[0];
+        int b = array[array.length-1];
+        int mod =  b%i;
+                return mod;
+
+    }
+
     public static void main(String[] args) {
         int[] array = {156, 486, 4678, 546, 646, 188, 674, 133, 987, 434};
         System.out.println(Sum(array));
+        System.out.println(Modulus(array));
     }
 }
