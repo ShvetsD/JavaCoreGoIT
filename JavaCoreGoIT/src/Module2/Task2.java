0dac07517d16f081d5de813849b9aa90b85e3fc4
AOC <ShvecD@i.ua>
2016-09-09 22:07:27 +0300

    Module2 task1.1 with method

--- JavaCoreGoIT/src/Module2/Task2.java
+++ JavaCoreGoIT/src/Module2/Task2.java
@@ -2,23 +2,26 @@ package Module2;
 
 public class Task2 {
     static double withdrawBalance(double balance, double withdrawal, double comission) {
-
-
         return balance - withdrawal - comission;
     }
 
+
     public static void main(String[] args) {
         int[] balances = {1200, 250, 2000, 500, 3200};
-        int[] balances1 = {200, 200, 500, 4000, 30};
-        double balance = 1500;
-        double withdrawal = 1450;
+        // int[] balances1 = {200, 200, 500, 4000, 30};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+        int i =1;
+            String ownerName = ownerNames[i];
+            double balance = balances[i];
+
+        double withdrawal = 100;
         double a = 0.05;
         double comission = withdrawal * a;
         double balanceAfter = withdrawBalance(balance, withdrawal, comission);
         if (balanceAfter >= 0) {
-            System.out.println("OK " + comission + " " + balanceAfter);
-        } else {
-            System.out.println("NO");
+            System.out.println(ownerName + " " + withdrawal + " " + balanceAfter);
         }
     }
 }
+
